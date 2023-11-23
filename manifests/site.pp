1e17e647189aa779df920d75913aed108b09a161
Fenikks <49687341+Fenikks@users.noreply.github.com>
2023-11-23 20:55:55 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,10 +2,17 @@ node default {
   package {'mysql':
     ensure => installed,
   }
+
+  notify {"Running os ${facts['os']['name']} version ${facts['os']['release']['full']}":}
 }
 
 node slave1.puppet {
   package {'htop':
     ensure => present,
+  }
+
+  file {'/tmp/os-info.txt':
+     ensure => file,
+     content => "Running os ${facts['os']['name']} version ${facts['os']['release']['full']}",
   }
 }
