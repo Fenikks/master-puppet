48a0a7f9d229e06899674c92c0d7f5c3aca95891
Fenikks <49687341+Fenikks@users.noreply.github.com>
2023-11-23 21:36:58 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,21 +4,6 @@ node default {
   }
 
   notify {"Running os ${facts['os']['name']} version ${facts['os']['release']['full']}":}
-
-#- name: Unpack application file
-#  shell: "gunzip -c /tmp/wcg.gz > {{ wcg_path }}/word-cloud-generator"
-#  when: get_file.changed == true
-#  notify: wcg-service
-
-#- name: Change application file permissions
-#  file:
-#    dest: "{{ wcg_path }}/word-cloud-generator"
-#    group: wcg
-#    owner: wcg
-#    mode: '0755'
-#  notify: wcg-service
-
- 
 }
 
 node slave1.puppet {
@@ -45,16 +30,39 @@ node slave1.puppet {
     ensure  => directory,
     owner => "wcg",
     group => "wcg",
-  
+    before => Exec['unpack wcg'],
   }
 
   file {'/tmp/wcg.gz':
     ensure => file,
     source => "https://github.com/Fenikks/devops-files-23.08/raw/master/word-cloud-generator.gz",
+    
   }
 
   file {'/etc/systemd/system/wcg.service':
     ensure => file,
     source => "https://raw.githubusercontent.com/Fenikks/master-puppet/production/files/wcg.service",
   }
+
+  exec {'unpack wcg':
+    command => "gunzip -c /tmp/wcg.gz > /opt/wordcloud/word-cloud-generator",
+    require => File['/tmp/wcg.gz'],
+  }
+
+  file {'/opt/wordcloud/word-cloud-generator':
+    owner => "wcg",
+    group => "wcg",
+    require => Exec['unpack wcg'],
+  }
+
+
+#- name: Change application file permissions
+#  file:
+#    dest: "{{ wcg_path }}/word-cloud-generator"
+#    group: wcg
+#    owner: wcg
+#    mode: '0755'
+#  notify: wcg-service
+
+
 }
