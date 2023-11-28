52345bc291a5ea6c00b191566bd12f124072bdbf
Fenikks <49687341+Fenikks@users.noreply.github.com>
2023-11-28 19:32:39 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,14 @@
 node default{
 
-  class dev_user {
-    user {'demouser':
-      ensure => present,
-      managehome => true,
-      groups => ['wheel'],
-      password => '$1$pWicQEb0$lGXc.RyHF7VAG7tKOpIap1'
-    }
-  }
+include dev_user
 
+}
+
+class dev_user {
+  user {'demouser':
+    ensure => present,
+    managehome => true,
+    groups => ['wheel'],
+    password => '$1$pWicQEb0$lGXc.RyHF7VAG7tKOpIap1'
+  }
 }
