2d4f67089fd1e632f17414ed8ac015dfad846428
Fenikks <49687341+Fenikks@users.noreply.github.com>
2023-11-28 19:36:56 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,7 @@
 node default{
 
 include dev_user
+include dev_editor
 
 }
 
@@ -10,5 +11,17 @@ class dev_user {
     managehome => true,
     groups => ['wheel'],
     password => '$1$pWicQEb0$lGXc.RyHF7VAG7tKOpIap1'
+  }
+}
+
+class dev_editor {
+  package {'vim':
+    ensure => present,
+  }
+  file {'/home/demouser/.vimrc':
+    ensure => file,
+    owner => "demouser",
+    group => "demouser",
+    content => "set number",
   }
 }
