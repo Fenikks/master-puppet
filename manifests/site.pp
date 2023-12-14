45aec4c6151d03a649a8a68d657b3cc0e7ad20e1
Alina Freydina <alina.f@reproto.com>
2023-12-14 21:11:06 +0300

    hw3,4

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,70 +1,46 @@
-node default {
-  include role::dev_machine
+node master.puppet {
+    firewalld_port { 'Open port 8080 in the public zone':
+        ensure   => present,
+        zone     => 'public',
+        port     => 8140,
+        protocol => 'tcp',
+    }
 }
 
 node slave1.puppet {
-  include role::web_server
+    package{'httpd':
+        ensure => installed,
+    }
+
+    service{ 'httpd':
+        ensure => running,
+        enable => true,
+    }
+
+    file { '/var/www/html/index.html'
+        ensure => file,
+        source => "https://raw.githubusercontent.com/Fenikks/devops-files-23.08/master/05-puppet/files/index.html"
+        replace => true
+    }
 }
 
-class wcg {
-  package {'htop':
-    ensure => present,
-  }
+node slave2.puppet {
+    package{['httpd','php']:
+        ensure => installed,
+    }
 
-  file {'/tmp/os-info.txt':
-     ensure => file,
-     content => "Running os ${facts['os']['name']} version ${facts['os']['release']['full']}",
-  }
+    service{ 'httpd':
+      ensure => running,
+      enable => true,
+    }
 
-  group {'wcg':
-    ensure => present,
-  }
+    file { '/var/www/html/index.php'
+        ensure => file,
+        source => "https://raw.githubusercontent.com/Fenikks/devops-files-23.08/master/05-puppet/files/index.php"
+        replace => true
+    }
+}
 
-  user {'wcg':
-    ensure => present,
-    groups => ['wcg'],
-    require => Group['wcg'],
-  }
-
-  file {'/opt/wordcloud':
-    ensure  => directory,
-    owner => "wcg",
-    group => "wcg",
-    before => Exec['unpack wcg'],
-  }
-
-  file {'/tmp/wcg.gz':
-    ensure => file,
-    source => "https://github.com/Fenikks/devops-files-23.08/raw/master/word-cloud-generator.gz",
-    
-  }
-
-  file {'/etc/systemd/system/wcg.service':
-    ensure => file,
-    source => "https://raw.githubusercontent.com/Fenikks/master-puppet/production/files/wcg.service",
-  }
-
-  exec {'unpack wcg':
-    command => "gunzip -c /tmp/wcg.gz > /opt/wordcloud/word-cloud-generator",
-    require => File['/tmp/wcg.gz'],
-    path => "/usr/bin",
-    notify => Service['wcg'],
-  }
-
-  file {'/opt/wordcloud/word-cloud-generator':
-    owner => "wcg",
-    group => "wcg",
-    mode => "755",
-    require => Exec['unpack wcg'],
-  }
-
-  service {'wcg':
-    ensure => running,
-    enable => true,
-  }
+# node manicraft.puppet {
   
-  service {'firewalld':
-    ensure => stopped,
-    enable => false,
-  }
-}
+# }
